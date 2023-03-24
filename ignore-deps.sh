fb8aaf2abb33e7aff031529c89db7206f309a60d
youkunlin20 <youkunlin20@mails.ucas.ac.cn>
2023-03-24 17:21:25 +0000

    ignore deps

---
+++ ignore-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read_dir_file(){
+for file in `ls $1`  # $1 是函数传进的参数
+do
+ if [ -d $1"/"$file ]   # -d判断是否是目录
+ then
+	read_dir_file $1"/"$file
+ else  #  在else部分处理文件
+	git update-index --skip-worktree $1"/"$file
+ fi
+done
+}
+read_dir_file deps/antlr4-runtime/dist
+read_dir_file deps/antlr4-runtime/build
